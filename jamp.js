70e8514438e81e4c43e93e02bfb604a50ef074e9
APIDT <144515341+APIDT@users.noreply.github.com>
2023-10-27 10:18:17 +0300

    Add files via upload

---
+++ jamp.js
@@ -0,0 +1,18 @@
+
+let boolean = true;
+let string = "best";
+let nul = NaN;
+let underf = undefined;
+let number = 190;
+
+console.table({ boolean, string, nul, underf, number });
+
+mydate();
+
+function mydate() {
+    let nickname = "Show";
+    let forename = "Jonh";
+    let old = 26;
+    let place = "North";
+    console.table({ nickname, forename, old, place });
+}
